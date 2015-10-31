ead38948c3a307301d61054dbfafbac199d29fc2
Anurag Uniyal <auniyal@vmware.com>
2015-10-30 23:38:46 -0700

    Separated bundles for external libs and app

--- gulpfile.js
+++ gulpfile.js
@@ -9,57 +9,99 @@ var child = require('child_process');
 var fs = require('fs');
 var debug = require('gulp-debug');
 var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
 
 var path = {
   COPY: ['src/index.html', 'src/css/style.css'],
   MINIFIED_OUT: 'build.min.js',
   OUT: 'build.js',
+  LIBS_OUT: 'libs.js',
   DEST: 'dist',
-  DEST_BUILD: 'dist/build',
   DEST_SRC: 'dist/src',
   ENTRY_POINT: './src/js/main.js'
 };
 
-gulp.task('copy', function(){
+function copy(){
   gulp.src(path.COPY)
     .pipe(gulp.dest(path.DEST));
-});
+}
+gulp.task('copy', copy);
 
-var watcher = undefined;
-function get_watcher(){
-  if(watcher) return watcher
-  watcher = watchify(browserify({
+var external_libs = ['react', 'react-dom', 'react-router', 'history', 'history/lib/createBrowserHistory'];
+
+function bundle_libs(){
+  var b = browserify({
+    debug: false//no source maps
+  });
+  external_libs.forEach(function(lib) {
+    b.require(lib);
+  });
+  b.bundle()
+   .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+   .on('log', gutil.log)
+   //.pipe(debug({title: 'unicorn:'}))
+   .pipe(source(path.LIBS_OUT))
+   .pipe(streamify(uglify()))
+   .pipe(gulp.dest(path.DEST_SRC))
+}
+
+function get_browserify(){
+  return browserify({
     entries: [path.ENTRY_POINT],
     transform: [reactify],
     debug: true,
     cache: {}, packageCache: {}, fullPaths: true
-  }));
-  watcher.on('log', gutil.log);
+  }).external(external_libs);
+  //.exclude('react)'
+}
+
+var watcher = undefined;
+function get_watcher(){
+  if(watcher) return watcher
+  watcher = get_browserify()
+      .plugin(watchify)
   return watcher;
 }
 
-function bundle(){
-  return get_watcher().bundle()
-      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-      //.pipe(debug({title: 'unicorn:'}))
-      .pipe(source(path.OUT))
-      .pipe(gulp.dest(path.DEST_SRC))
+function _bundle(b){
+  b.bundle()
+  .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+  .on('log', gutil.log)
+  //.pipe(debug({title: 'unicorn:'}))
+  .pipe(source(path.OUT))
+  .pipe(gulp.dest(path.DEST_SRC))
 }
 
-gulp.task('bundle', bundle)
+function incremental_bundle(){
+  gutil.log("incremental_bundle")
+  _bundle(get_watcher())
+}
+
+function full_bundle(){
+  _bundle(get_browserify())
+}
+
+gulp.task('bundle-app', full_bundle)
+
+gulp.task('bundle-libs', bundle_libs)
+
+gulp.task('bundle', ['bundle-app', 'bundle-libs'])
 
 gulp.task('watch', function() {
   gulp.watch(path.COPY, ['copy']);
+  copy() //copy at start
 
-  return get_watcher().on('update', bundle)
-
+  get_watcher().on('update', incremental_bundle)
+  // start the watchify bundler, first time, otherwise no updates are sent
+  incremental_bundle()
 });
 
 gulp.task('server', function() {
   var server = child.spawn('python', ['-m', 'SimpleHTTPServer']);
+  gutil.log("Starting server @ localhost:8000")
   var log = fs.createWriteStream('/tmp/server.log', {flags: 'a'});
   server.stdout.pipe(log);
   server.stderr.pipe(log);
 });
 
-gulp.task('default', ['watch', 'copy', 'bundle', 'server']);
+gulp.task('default', ['watch',  'server']);
